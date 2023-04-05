dec7bb565d38ab5aeee639b8fe950b5b443446ab
levynkhs <levynkhs2001@gmail.com>
2023-04-06 00:51:18 +0400

    login text box implemented, screen pushed to top when keyboard is used

--- lib/screen/auth_screen.dart
+++ lib/screen/auth_screen.dart
@@ -8,53 +8,85 @@ class AuthScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xfffcfeff),
-      body: Column(
-        children: [
-          Container(
-            height: MediaQuery.of(context).padding.top + kToolbarHeight,
-            padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-            child: const Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: EdgeInsets.only(left: 16.0),
-                child: Text(
-                  'Welcome,',
-                  style: TextStyle(
-                    color: Colors.black, // The color of the text
-                    fontSize: 40.0, // The size of the text
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).padding.top + kToolbarHeight,
+              padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+              child: const Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: EdgeInsets.only(left: 16.0),
+                  child: Text(
+                    'Welcome,',
+                    style: TextStyle(
+                      color: Colors.black, // The color of the text
+                      fontSize: 40.0, // The size of the text
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(height: 0.0),
-          Image.asset(
-            'images/workout.jpg',
-            height: 300.0,
-          ),
-          Expanded(
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.all(20),
+            const SizedBox(height: 0.0),
+            Image.asset(
+              'images/workout.jpg',
+              height: 300.0,
+            ),
+            const SizedBox(height: 10.0),
+            SizedBox(
+              height: 400,
+              width: 350,
+              child: Container(
+                margin: EdgeInsets.all(16.0),
+                padding: EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Color.fromARGB(255, 200, 200, 200),
+                    width: 3.0,
+                  ),
+                  color: Color.fromARGB(255, 214, 228, 239),
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    const Text(
-                      "Select Auth provider",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                    AuthButton(
-                      iconData: Icons.email,
-                      tittle: "Email/Password",
-                      onTap: () {},
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Username',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(),
+                      ),
                     ),
                   ],
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
