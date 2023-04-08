4094c7710dce8e1693110ab4244e3c1d95d934c4
levynkhs <levynkhs2001@gmail.com>
2023-04-08 17:40:41 +0400

    put box slider

--- lib/screen/term_and_condition.dart
+++ lib/screen/term_and_condition.dart
@@ -86,7 +86,7 @@ class Terms_and_condition extends StatelessWidget {
                   ),
                   const SizedBox(height: 0.0),
                   SizedBox(
-                    height: 1800,
+                    height: 680,
                     width: 370,
                     child: Stack(
                       children: [
@@ -114,111 +114,116 @@ class Terms_and_condition extends StatelessWidget {
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               const SizedBox(height: 0.0),
-                              SingleChildScrollView(
-                                scrollDirection: Axis.vertical,
-                                child: Text.rich(
-                                  TextSpan(
-                                    children: [
+                              Expanded(
+                                child: SingleChildScrollView(
+                                  scrollDirection: Axis.vertical,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(16.0),
+                                    child: Text.rich(
                                       TextSpan(
-                                        text: 'TERMS AND CONDITIONS\n\n',
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 20.0,
-                                        ),
+                                        children: [
+                                          TextSpan(
+                                            text: 'TERMS AND CONDITIONS\n\n',
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 20.0,
+                                            ),
+                                          ),
+                                          TextSpan(
+                                            text:
+                                                'Welcome to our Nutritionist App! This app has been designed to provide you with useful nutritional advice and guidance, but before you use it, please take a few minutes to read our terms and conditions carefully. By using our app, you are agreeing to be bound by these terms and conditions, so if you don\'t agree with them, please don\'t use our app.\n\n',
+                                            style: TextStyle(fontSize: 16.0),
+                                          ),
+                                          TextSpan(
+                                            text: 'DEFINITIONS\n',
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 18.0,
+                                            ),
+                                          ),
+                                          TextSpan(
+                                            text:
+                                                'In these terms and conditions, the following definitions apply:\n',
+                                            style: TextStyle(fontSize: 16.0),
+                                          ),
+                                          TextSpan(
+                                            text:
+                                                '"App" means the Nutritionist App.\n"User" means any person who uses the App.\n"Content" means all information, text, graphics, images, videos, and other material that is provided through the App.\n"Nutritionist" means any person who provides nutritional advice through the App.\n\n',
+                                            style: TextStyle(fontSize: 16.0),
+                                          ),
+                                          TextSpan(
+                                            text: 'USE OF THE APP\n',
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 18.0,
+                                            ),
+                                          ),
+                                          TextSpan(
+                                            text:
+                                                'The App is intended for informational purposes only and should not be used as a substitute for professional medical advice, diagnosis, or treatment. The Nutritionist App does not provide medical advice, and any information or content provided through the App should not be considered medical advice.\nThe App is available only to individuals who are at least 18 years old. If you are under 18 years old, you may not use the App.\n\n',
+                                            style: TextStyle(fontSize: 16.0),
+                                          ),
+                                          TextSpan(
+                                            text:
+                                                'The Nutritionist App may require you to create an account to use certain features. You must provide accurate and complete information when creating your account, and you are responsible for maintaining the security of your account.\n\n',
+                                            style: TextStyle(fontSize: 16.0),
+                                          ),
+                                          TextSpan(
+                                            text: 'CONTENT\n',
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 18.0,
+                                            ),
+                                          ),
+                                          TextSpan(
+                                            text:
+                                                'All content provided through the App is provided for informational purposes only. The Nutritionist App does not guarantee the accuracy, completeness, or usefulness of any content provided through the App, and the Nutritionist App is not responsible for any errors or omissions in the content.\n\n',
+                                            style: TextStyle(fontSize: 16.0),
+                                          ),
+                                          TextSpan(
+                                            text: 'NUTRITIONIST SERVICES\n',
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 18.0,
+                                            ),
+                                          ),
+                                          TextSpan(
+                                            text:
+                                                'The Nutritionist App provides a platform for users to connect with Nutritionists who can provide nutritional advice. The Nutritionists are not employees or agents of the Nutritionist App, and the Nutritionist App does not endorse any Nutritionist or their advice.\n\n',
+                                            style: TextStyle(fontSize: 16.0),
+                                          ),
+                                          TextSpan(
+                                            text:
+                                                'Any agreement for nutritional advice is solely between the User and the Nutritionist. The Nutritionist App is not a party to any such agreement, and the Nutritionist App has no responsibility or liability for any advice or services provided by the Nutritionist.\n\n',
+                                            style: TextStyle(fontSize: 16.0),
+                                          ),
+                                          TextSpan(
+                                            text: 'FEES\n',
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 18.0,
+                                            ),
+                                          ),
+                                          TextSpan(
+                                            text:
+                                                'The Nutritionist App may charge fees for certain features or services, such as access to premium content or nutritional advice from a Nutritionist. If you choose to use any fee-based features or services, you agree to pay all applicable fees.\n\n',
+                                            style: TextStyle(fontSize: 16.0),
+                                          ),
+                                          TextSpan(
+                                            text: 'GOVERNING LAW\n',
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 18.0,
+                                            ),
+                                          ),
+                                          TextSpan(
+                                            text:
+                                                'These terms and conditions are governed by the laws of the state in which the Nutritionist App is headquartered.\n\n',
+                                            style: TextStyle(fontSize: 16.0),
+                                          ),
+                                        ],
                                       ),
-                                      TextSpan(
-                                        text:
-                                            'Welcome to our Nutritionist App! This app has been designed to provide you with useful nutritional advice and guidance, but before you use it, please take a few minutes to read our terms and conditions carefully. By using our app, you are agreeing to be bound by these terms and conditions, so if you don\'t agree with them, please don\'t use our app.\n\n',
-                                        style: TextStyle(fontSize: 16.0),
-                                      ),
-                                      TextSpan(
-                                        text: 'DEFINITIONS\n',
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 18.0,
-                                        ),
-                                      ),
-                                      TextSpan(
-                                        text:
-                                            'In these terms and conditions, the following definitions apply:\n',
-                                        style: TextStyle(fontSize: 16.0),
-                                      ),
-                                      TextSpan(
-                                        text:
-                                            '"App" means the Nutritionist App.\n"User" means any person who uses the App.\n"Content" means all information, text, graphics, images, videos, and other material that is provided through the App.\n"Nutritionist" means any person who provides nutritional advice through the App.\n\n',
-                                        style: TextStyle(fontSize: 16.0),
-                                      ),
-                                      TextSpan(
-                                        text: 'USE OF THE APP\n',
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 18.0,
-                                        ),
-                                      ),
-                                      TextSpan(
-                                        text:
-                                            'The App is intended for informational purposes only and should not be used as a substitute for professional medical advice, diagnosis, or treatment. The Nutritionist App does not provide medical advice, and any information or content provided through the App should not be considered medical advice.\nThe App is available only to individuals who are at least 18 years old. If you are under 18 years old, you may not use the App.\n\n',
-                                        style: TextStyle(fontSize: 16.0),
-                                      ),
-                                      TextSpan(
-                                        text:
-                                            'The Nutritionist App may require you to create an account to use certain features. You must provide accurate and complete information when creating your account, and you are responsible for maintaining the security of your account.\n\n',
-                                        style: TextStyle(fontSize: 16.0),
-                                      ),
-                                      TextSpan(
-                                        text: 'CONTENT\n',
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 18.0,
-                                        ),
-                                      ),
-                                      TextSpan(
-                                        text:
-                                            'All content provided through the App is provided for informational purposes only. The Nutritionist App does not guarantee the accuracy, completeness, or usefulness of any content provided through the App, and the Nutritionist App is not responsible for any errors or omissions in the content.\n\n',
-                                        style: TextStyle(fontSize: 16.0),
-                                      ),
-                                      TextSpan(
-                                        text: 'NUTRITIONIST SERVICES\n',
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 18.0,
-                                        ),
-                                      ),
-                                      TextSpan(
-                                        text:
-                                            'The Nutritionist App provides a platform for users to connect with Nutritionists who can provide nutritional advice. The Nutritionists are not employees or agents of the Nutritionist App, and the Nutritionist App does not endorse any Nutritionist or their advice.\n\n',
-                                        style: TextStyle(fontSize: 16.0),
-                                      ),
-                                      TextSpan(
-                                        text:
-                                            'Any agreement for nutritional advice is solely between the User and the Nutritionist. The Nutritionist App is not a party to any such agreement, and the Nutritionist App has no responsibility or liability for any advice or services provided by the Nutritionist.\n\n',
-                                        style: TextStyle(fontSize: 16.0),
-                                      ),
-                                      TextSpan(
-                                        text: 'FEES\n',
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 18.0,
-                                        ),
-                                      ),
-                                      TextSpan(
-                                        text:
-                                            'The Nutritionist App may charge fees for certain features or services, such as access to premium content or nutritional advice from a Nutritionist. If you choose to use any fee-based features or services, you agree to pay all applicable fees.\n\n',
-                                        style: TextStyle(fontSize: 16.0),
-                                      ),
-                                      TextSpan(
-                                        text: 'GOVERNING LAW\n',
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 18.0,
-                                        ),
-                                      ),
-                                      TextSpan(
-                                        text:
-                                            'These terms and conditions are governed by the laws of the state in which the Nutritionist App is headquartered.\n\n',
-                                        style: TextStyle(fontSize: 16.0),
-                                      ),
-                                    ],
+                                    ),
                                   ),
                                 ),
                               ),
@@ -236,6 +241,7 @@ class Terms_and_condition extends StatelessWidget {
                                   Text('Agree terms and conditions'),
                                 ],
                               ),
+                              const SizedBox(height: 35.0),
                             ],
                           ),
                         ),
