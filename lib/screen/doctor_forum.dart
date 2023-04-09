44f5eded4dfb7d3d817fec2c013028ae9d35bb62
derek_lee <20820343@student.curtin.edu.au>
2023-04-09 22:02:59 +0800

    feat: Added TitleBar and refactored code

--- lib/screen/doctor_forum.dart
+++ lib/screen/doctor_forum.dart
@@ -6,7 +6,7 @@ class DoctorForum extends StatelessWidget {
   static const double _borderRadius = 23;
   static const double _textFactor = 1.3;
   static const double _iconSize = 30;
-  static const double _boxWidth = 300;
+  static const double _boxWidth = 325;
   static const double _boxHeight = 75;
   static const dynamic _textColour = Colors.black;
   static const dynamic _iconColour = Colors.grey;
@@ -24,6 +24,7 @@ class DoctorForum extends StatelessWidget {
               color: Color.fromARGB(255, 203, 222, 250),
             ),
           ),
+          _buildTitleBar(),
           Positioned(
             left: 0,
             right: 0,
@@ -31,105 +32,121 @@ class DoctorForum extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(_borderRadius),
-                  child: SizedBox(
-                    width: _boxWidth,
-                    height: _boxHeight,
-                    child: ElevatedButton.icon(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.calendar_month_outlined,
-                        size: _iconSize,
-                        color: _iconColour[600],
-                      ),
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            Color.fromARGB(255, 173, 184, 252)),
-                      ),
-                      label: RichText(
-                        textAlign: TextAlign.center,
-                        text: TextSpan(
-                          text: 'Book an Appointment\n',
-                          style: TextStyle(
-                              color: _textColour,
-                              fontSize: 19,
-                              fontWeight: FontWeight.w500),
-                          children: <TextSpan>[
-                            TextSpan(
-                              text: 'Available 9:00 to 11:00 a.m',
-                              style: TextStyle(
-                                  color: _textColour,
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.normal),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
+                _buildAppointmentButton(),
                 SizedBox(height: 30),
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(_borderRadius),
-                  child: SizedBox(
-                    width: _boxWidth,
-                    height: _boxHeight,
-                    child: ElevatedButton.icon(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.chat_rounded,
-                        size: _iconSize,
-                        color: _iconColour[600],
-                      ),
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            Color.fromARGB(255, 173, 184, 252)),
-                      ),
-                      label: Text(
-                        'Chat With Doctor',
-                        textScaleFactor: _textFactor,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: _textColour,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
+                _buildElevatedButton(
+                    'Chat With Doctor', Icons.chat_rounded, () {}),
                 SizedBox(height: 30),
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(_borderRadius),
-                  child: SizedBox(
-                    width: _boxWidth,
-                    height: _boxHeight,
-                    child: ElevatedButton.icon(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.chat,
-                        size: _iconSize,
-                        color: _iconColour[600],
-                      ),
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            Color.fromARGB(255, 173, 184, 252)),
-                      ),
-                      label: Text(
-                        'Chat With Friends',
-                        textScaleFactor: _textFactor,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: _textColour,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
+                _buildElevatedButton('Chat With Friends', Icons.chat, () {}),
                 SizedBox(height: 50),
               ],
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildTitleBar() {
+    return Container(
+      margin: EdgeInsets.only(top: 20, left: 25, right: 25),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(_borderRadius),
+        child: SizedBox(
+          height: 55,
+          child: Container(
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 226, 226, 226),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.person_2_outlined,
+                  size: 30,
+                ),
+                SizedBox(width: 15),
+                Text(
+                  "Doctor's Forum",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildElevatedButton(
+      String text, IconData icon, Function() onPressed) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(_borderRadius),
+      child: SizedBox(
+        width: _boxWidth,
+        height: _boxHeight,
+        child: ElevatedButton.icon(
+          onPressed: onPressed,
+          icon: Icon(
+            icon,
+            size: _iconSize,
+            color: _iconColour[600],
+          ),
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(
+                Color.fromARGB(255, 173, 184, 252)),
+          ),
+          label: Text(
+            text,
+            textScaleFactor: _textFactor,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: _textColour,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAppointmentButton() {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(_borderRadius),
+      child: SizedBox(
+        width: _boxWidth,
+        height: _boxHeight,
+        child: ElevatedButton.icon(
+          onPressed: () {},
+          icon: Icon(
+            Icons.calendar_month_outlined,
+            size: _iconSize,
+            color: _iconColour[600],
+          ),
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(
+                Color.fromARGB(255, 173, 184, 252)),
+          ),
+          label: RichText(
+            textAlign: TextAlign.center,
+            text: TextSpan(
+              text: 'Book an Appointment\n',
+              style: TextStyle(
+                  color: _textColour,
+                  fontSize: 19,
+                  fontWeight: FontWeight.w500),
+              children: <TextSpan>[
+                TextSpan(
+                  text: 'Available 9:00 to 11:00 a.m',
+                  style: TextStyle(
+                      color: _textColour,
+                      fontSize: 15,
+                      fontWeight: FontWeight.normal),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
