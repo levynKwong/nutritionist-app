007277d2e9f1c9a4827ca52285ffd917f2250c66
levynkhs <levynkhs2001@gmail.com>
2023-04-09 18:44:19 +0400

    put them in folders

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:meal_aware/screen/auth/auth_screen.dart';
 import 'package:meal_aware/screen/auth/auth_screen_register.dart';
 import 'package:meal_aware/screen/auth/term_and_condition.dart';
 import 'package:meal_aware/screen/home/home.dart';
+import 'package:meal_aware/screen/home/doctor_forum.dart';
 
 import 'firebase_options.dart';
 
@@ -37,10 +38,11 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // home: RegisterScreen(),
-      home: AuthScreen(),
+      // home: AuthScreen(),
       // home: const ParentAuth(),
       // home: const Terms_and_condition()
       // home: test_home(),
+      home: DoctorForum(),
     );
   }
 }
