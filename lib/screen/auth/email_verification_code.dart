87f56f5d77bc851f5d6d6becfc7895c617aa2c23
levynkhs <levynkhs2001@gmail.com>
2023-04-12 16:33:27 +0400

    initialized email verification

---
+++ lib/screen/auth/email_verification_code.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class EmailVerificationCode extends StatefulWidget {
+  const EmailVerificationCode({Key? key}) : super(key: key);
+
+  @override
+  _EmailVerificationCodeState createState() => _EmailVerificationCodeState();
+}
+
+class _EmailVerificationCodeState extends State<EmailVerificationCode> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold();
+  }
+}
