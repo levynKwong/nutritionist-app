6ec3486fadc24301e4d2066f2aa3f6c394cb9557
levynkhs <levynkhs2001@gmail.com>
2023-04-27 15:20:11 +0400

    started doing nutrionist dashboard

--- lib/screen/nutritionist_home/dashboard/dashboard.dart
+++ lib/screen/nutritionist_home/dashboard/dashboard.dart
@@ -18,6 +18,154 @@ class _dashboardState extends State<dashboard> {
       child: Column(
         children: [
           appBar(width_),
+          SizedBox(
+            height: height_ * 0.0,
+          ),
+          info(width_, height_),
+          SizedBox(
+            height: height_ * 0.0,
+          ),
+          client(width_, height_),
+        ],
+      ),
+    );
+  }
+
+  Widget client(double width_, double height_) {
+    return SizedBox(
+      height: height_ * 0.55,
+      width: width_ * 1.3,
+      child: Container(
+        margin: EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 255, 255, 255),
+          borderRadius: BorderRadius.circular(20.0),
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromARGB(255, 207, 207, 207).withOpacity(0.3),
+              spreadRadius: 3,
+              blurRadius: 2,
+              offset: Offset(0, 1),
+            ),
+          ],
+        ),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget info(double width_, double height_) {
+    return Container(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              SizedBox(
+                width: width_ * 0.04,
+              ),
+              Container(
+                width: width_ * 0.44,
+                height: height_ * 0.12,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color.fromARGB(255, 255, 255, 255),
+                ),
+                child: Center(
+                  child: Text(
+                    'Hello',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 0, 0, 0),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: width_ * 0.03,
+              ),
+              Container(
+                width: width_ * 0.44,
+                height: height_ * 0.12,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color.fromARGB(255, 255, 255, 255),
+                ),
+                child: Center(
+                  child: Text(
+                    'Hello',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 0, 0, 0),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: width_ * 0.04,
+              ),
+            ],
+          ),
+          SizedBox(
+            height: height_ * 0.02,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: width_ * 0.04,
+              ),
+              Container(
+                width: width_ * 0.44,
+                height: height_ * 0.12,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color.fromARGB(255, 255, 255, 255),
+                ),
+                child: Center(
+                  child: Text(
+                    'Hello',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 0, 0, 0),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: width_ * 0.03,
+              ),
+              Container(
+                width: width_ * 0.44,
+                height: height_ * 0.12,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color.fromARGB(255, 255, 255, 255),
+                ),
+                child: Center(
+                  child: Text(
+                    'Hello',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 0, 0, 0),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: width_ * 0.04,
+              ),
+            ],
+          )
         ],
       ),
     );
