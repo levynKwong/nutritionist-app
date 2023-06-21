0b6e41ac094e9d56891b558dc829fbd5eb8c7c50
levynkhs <96076166+levynkwong2001@users.noreply.github.com>
2023-06-21 20:06:55 +0400

    payment not able to implement, 1 mil

--- lib/screen/home/profile/BuyToken/BuyCoin.dart
+++ lib/screen/home/profile/BuyToken/BuyCoin.dart
@@ -7,7 +7,6 @@ import 'package:meal_aware/screen/home/home_screen.dart';
 import 'package:meal_aware/screen/home/profile/BuyToken/api/purchase_api.dart';
 import 'package:purchases_flutter/purchases_flutter.dart';
 
-
 class BuyCoin extends StatefulWidget {
   const BuyCoin({Key? key}) : super(key: key);
 
@@ -16,15 +15,9 @@ class BuyCoin extends StatefulWidget {
 }
 
 class _BuyCoinState extends State<BuyCoin> {
-  int selectedRadio = 0;
-  String coin1Id = 'coin_1';
-  String coin2Id = 'coin_2';
-  String coin3Id = 'coin_3';
-
   @override
   void initState() {
     super.initState();
-    selectedRadio = 0;
     _initialize();
   }
 
@@ -32,59 +25,33 @@ class _BuyCoinState extends State<BuyCoin> {
     await PurchaseApi.init();
   }
 
-  setSelectedRadio(int val) {
-    setState(() {
-      selectedRadio = val;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final double width_ = MediaQuery.of(context).size.width;
-    final double height_ = MediaQuery.of(context).size.height;
-
-    return Scaffold(
-      body: Stack(
-        children: [
-          background(),
-          SingleChildScrollView(
-            child: SafeArea(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  topBar(width_, height_, context),
-                  CoinCounter(),
-                  SizedBox(
-                    height: height_ * 0.06,
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(
-                      left: width_ * 0.1,
-                      right: width_ * 0.1,
-                    ),
-                    child: Text(
-                      'You can use the coins to communicate with your nutritionist and book appointments with them.',
-                    ),
-                  ),
-                  SizedBox(
-                    height: height_ * 0.06,
-                  ),
-                  selectionButton(height_, width_),
-                  SizedBox(
-                    height: height_ * 0.02,
-                  ),
-                  TermsofUse(height_, width_),
-                  SizedBox(
-                    height: height_ * 0.02,
-                  ),
-                  buttons(height_, width_),
-                ],
-              ),
-            ),
+  Future<void> _buyCoins(String coinId) async {
+    try {
+      final purchaserInfo = await PurchaseApi.purchaseCoins(coinId);
+      if (purchaserInfo != null) {
+        // Handle the purchased package or update the UI accordingly
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Purchase successful.'),
+            backgroundColor: Colors.green,
           ),
-        ],
-      ),
-    );
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Purchase failed.'),
+            backgroundColor: Colors.red,
+          ),
+        );
+      }
+    } on PlatformException catch (error) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Purchase failed: ${error.message}'),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
   }
 
   Widget buttons(double height_, double width_) {
@@ -94,7 +61,9 @@ class _BuyCoinState extends State<BuyCoin> {
         children: [
           SizedBox(width: width_ * 0.15),
           ElevatedButton(
-            onPressed: _buyCoins,
+            onPressed: () {
+              _buyCoins('coin_2');
+            },
             style: ElevatedButton.styleFrom(
               foregroundColor: Colors.white,
               backgroundColor: Color(0xFF575ecb),
@@ -109,177 +78,187 @@ class _BuyCoinState extends State<BuyCoin> {
     );
   }
 
-  Center TermsofUse(double height_, double width_) {
-    return Center(
-      child: Container(
-        margin: EdgeInsets.only(left: width_ * 0.1, right: width_ * 0.1),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextButton(
-              onPressed: () {
-                // Navigate to Terms of Use page
-              },
-              child: Text(
-                'Terms of Use',
-                style: TextStyle(
-                  color: Color(0xFF7B7B7B),
-                ),
-              ),
-            ),
-            Text(
-              ' | ',
-              style: TextStyle(
-                color: Color(0xFF7B7B7B),
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-                // Navigate to Privacy Policy page
-              },
-              child: Text(
-                'Privacy Policy',
-                style: TextStyle(
-                  color: Color(0xFF7B7B7B),
-                ),
-              ),
-            ),
-          ],
-        ),
+  @override
+  Widget build(BuildContext context) {
+    // Your widget build code here
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Buy Coin'),
+      ),
+      body: Center(
+        child: buttons(MediaQuery.of(context).size.height, MediaQuery.of(context).size.width),
       ),
     );
   }
+}
 
-  Widget selectionButton(double height_, double width_) {
-    return Center(
-      child: Container(
-        child: Container(
-          width: width_ * 0.8,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              buildRadioTile(
-                value: 1,
-                image: Image.asset('images/token1.png'),
-                title: Text('1', style: TextStyle(fontWeight: FontWeight.bold)),
-                imageSize: width_ * 0.11,
-              ),
-              SizedBox(height: height_ * 0.02),
-              buildRadioTile(
-                value: 2,
-                image: Image.asset('images/token2.png'),
-                title: Text('2', style: TextStyle(fontWeight: FontWeight.bold)),
-                imageSize: width_ * 0.13,
-              ),
-              SizedBox(height: height_ * 0.02),
-              buildRadioTile(
-                value: 3,
-                image: Image.asset('images/token3.png'),
-                title: Text('3', style: TextStyle(fontWeight: FontWeight.bold)),
-                imageSize: width_ * 0.14,
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+class PurchaseApi {
+  static const _apiKey = 'goog_JOqwYSmDCvNhYRFmMfOVybysoDu';
+
+  static Future<void> init() async {
+    await Purchases.setDebugLogsEnabled(true);
+    await Purchases.setup(_apiKey);
   }
 
-  Widget buildRadioTile({
-    required int value,
-    required Image image,
-    required Widget title,
-    required double imageSize,
-  }) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        color: Color(0xFFa0b1f9),
-      ),
-      height: 60,
-      child: RadioListTile(
-        value: value,
-        groupValue: selectedRadio,
-        onChanged: (val) {
-          print('number $val');
-          setSelectedRadio(val!);
-        },
-        controlAffinity: ListTileControlAffinity.trailing,
-        activeColor: Color(0xFF575dcb),
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              width: imageSize,
-              height: imageSize,
-              child: image,
-            ),
-            SizedBox(width: 26),
-            Flexible(child: title),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget topBar(double width_, double height_, BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () => Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => Home()),
-          ),
-        ),
-        Row(
-          children: [
-            Image.asset(
-              'images/tokenIcon.png',
-              height: height_ * 0.1,
-              width: width_ * 0.1,
-              color: Theme.of(context).colorScheme.secondary,
-            ),
-            SizedBox(
-              width: width_ * 0.08,
-            ),
-            Text('Buy Coin')
-          ],
-        ),
-        SizedBox(
-          width: width_ * 0.09,
-        ),
-      ],
-    );
-  }
-
-  void _buyCoins() async {
-  try {
-    final offerings = await PurchaseApi.fetchOffers();
-    if (offerings.isNotEmpty && selectedRadio > 0 && selectedRadio <= offerings.length) {
-      final offering = offerings[0];
-      final package = offering.availablePackages[selectedRadio - 1];
-
-      final purchaserInfo = await Purchases.purchasePackage(package);
-      // Handle the purchased package or update UI accordingly
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Invalid selection.'),
-          backgroundColor: Colors.red,
-        ),
-      );
+  static Future<List<Offering>> fetchOffersByIds(List<String> ids) async {
+    try {
+      final offerings = await Purchases.getOfferings();
+      return offerings.all.values.where((offer) => ids.contains(offer.identifier)).toList();
+    } on PlatformException catch (e) {
+      print(e);
+      return [];
     }
-  } on PlatformException catch (error) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text('Purchase failed: ${error.message}'),
-        backgroundColor: Colors.red,
-      ),
-    );
+  }
+
+  static Future<List<Offering>> fetchOffers({bool all = true}) async {
+    try {
+      final offerings = await Purchases.getOfferings();
+      if (all) {
+        final current = offerings.current;
+        return current == null ? [] : [current];
+      } else {
+        return offerings.all.values.toList();
+      }
+    } on PlatformException catch (e) {
+      print(e);
+      return [];
+    }
+  }
+
+  static Future purchaseCoins(String coinId) async {
+    try {
+      print('coinId: $coinId');
+      final offerings = await fetchOffersByIds([coinId]);
+      if (offerings.isNotEmpty) {
+        final offering = offerings.first;
+        final purchasePackage = offering.availablePackages.first;
+        final purchaserInfo = await Purchases.purchasePackage(purchasePackage);
+        return purchaserInfo;
+      } else {
+        print('No available offerings for coinId: $coinId');
+        return null;
+      }
+    } on PlatformException catch (e) {
+      print(e);
+      return null;
+    }
   }
 }
 
-}
+// goog_JOqwYSmDCvNhYRFmMfOVybysoDu
+ // Widget buildRadioTile({
+  //   required int value,
+  //   required Image image,
+  //   required Widget title,
+  //   required double imageSize,
+  // }) {
+  //   return Container(
+  //     decoration: BoxDecoration(
+  //       borderRadius: BorderRadius.circular(10.0),
+  //       color: Color(0xFFa0b1f9),
+  //     ),
+  //     height: 60,
+  //     child: RadioListTile(
+  //       value: value,
+  //       groupValue: selectedRadio,
+  //       onChanged: (val) {
+  //         print('number $val');
+  //         setSelectedRadio(val!);
+  //       },
+  //       controlAffinity: ListTileControlAffinity.trailing,
+  //       activeColor: Color(0xFF575dcb),
+  //       title: Row(
+  //         mainAxisAlignment: MainAxisAlignment.center,
+  //         children: [
+  //           SizedBox(
+  //             width: imageSize,
+  //             height: imageSize,
+  //             child: image,
+  //           ),
+  //           SizedBox(width: 26),
+  //           Flexible(child: title),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
+
+
+
+
+  // Widget selectionButton(double height_, double width_) {
+  //   return Center(
+  //     child: Container(
+  //       child: Container(
+  //         width: width_ * 0.8,
+  //         child: Column(
+  //           mainAxisAlignment: MainAxisAlignment.center,
+  //           crossAxisAlignment: CrossAxisAlignment.stretch,
+  //           children: <Widget>[
+  //             buildRadioTile(
+  //               value: 1,
+  //               image: Image.asset('images/token1.png'),
+  //               title: Text('1', style: TextStyle(fontWeight: FontWeight.bold)),
+  //               imageSize: width_ * 0.11,
+  //             ),
+  //             SizedBox(height: height_ * 0.02),
+  //             buildRadioTile(
+  //               value: 2,
+  //               image: Image.asset('images/token2.png'),
+  //               title: Text('2', style: TextStyle(fontWeight: FontWeight.bold)),
+  //               imageSize: width_ * 0.13,
+  //             ),
+  //             SizedBox(height: height_ * 0.02),
+  //             buildRadioTile(
+  //               value: 3,
+  //               image: Image.asset('images/token3.png'),
+  //               title: Text('3', style: TextStyle(fontWeight: FontWeight.bold)),
+  //               imageSize: width_ * 0.14,
+  //             ),
+  //           ],
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
+// --------------------------------------------
+  // Center TermsofUse(double height_, double width_) {
+  //   return Center(
+  //     child: Container(
+  //       margin: EdgeInsets.only(left: width_ * 0.1, right: width_ * 0.1),
+  //       child: Row(
+  //         mainAxisAlignment: MainAxisAlignment.center,
+  //         children: [
+  //           TextButton(
+  //             onPressed: () {
+  //               // Navigate to Terms of Use page
+  //             },
+  //             child: Text(
+  //               'Terms of Use',
+  //               style: TextStyle(
+  //                 color: Color(0xFF7B7B7B),
+  //               ),
+  //             ),
+  //           ),
+  //           Text(
+  //             ' | ',
+  //             style: TextStyle(
+  //               color: Color(0xFF7B7B7B),
+  //             ),
+  //           ),
+  //           TextButton(
+  //             onPressed: () {
+  //               // Navigate to Privacy Policy page
+  //             },
+  //             child: Text(
+  //               'Privacy Policy',
+  //               style: TextStyle(
+  //                 color: Color(0xFF7B7B7B),
+  //               ),
+  //             ),
+  //           ),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
--- lib/screen/home/profile/BuyToken/api/purchase_api.dart
+++ lib/screen/home/profile/BuyToken/api/purchase_api.dart
@@ -2,24 +2,47 @@ import 'package:flutter/services.dart';
 import 'package:purchases_flutter/models/offering_wrapper.dart';
 import 'package:purchases_flutter/purchases_flutter.dart';
 // goog_JOqwYSmDCvNhYRFmMfOVybysoDu
-class PurchaseApi {
-  static const _apiKey='goog_JOqwYSmDCvNhYRFmMfOVybysoDu';
 
-  static Future init() async {
-    await Purchases.setDebugLogsEnabled(true);
-    await Purchases.setup(_apiKey);
-  }
 
-  static Future<List<Offering>> fetchOffers() async {
-    try{
-      final offerings = await Purchases.getOfferings();
-      final current = offerings.current;
-      return current == null? [] : [current];
-    }on PlatformException catch (e){
-      
-      return [];
-    }
-  }
- 
-}
+// class PurchaseApi {
+//   static const _apiKey = '';
 
+//   static Future<void> init() async {
+//     await Purchases.setDebugLogsEnabled(true);
+//     await Purchases.setup(_apiKey);
+//   }
+
+//   static Future<List<Offering>> fetchOffersByIds(List<String> ids) async {
+//     final offers = await fetchOffers();
+//     return offers.where((offer) => ids.contains(offer.identifier)).toList();
+//   }
+
+//   static Future<List<Offering>> fetchOffers({bool all = true}) async {
+//     try {
+//       final offerings = await Purchases.getOfferings();
+//       if (all) {
+//         final current = offerings.current;
+//         return current == null ? [] : [current];
+//       } else {
+//         return offerings.all.values.toList();
+//       }
+//     } on PlatformException catch (e) {
+//       print(e);
+//       return [];
+//     }
+//   }
+
+//   static purchaseCoins(String coinId) async {
+//     try {
+//       print('coinId: $coinId');
+//       final offering = (await fetchOffersByIds([coinId])).first;
+//       final purchasePackage = offering.availablePackages.first;
+
+//       final purchaserInfo = await Purchases.purchasePackage(purchasePackage);
+//       return purchaserInfo;
+//     } on PlatformException catch (e) {
+//       print(e);
+//       return null;
+//     }
+//   }
+// }
